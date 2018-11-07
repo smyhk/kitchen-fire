5ea77137ab7e0ca8b97d01bf810d6c880e6d9f3b
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-11-07 09:42:13 -0800

    Implement basic actions

---
+++ src/store/actions/authActions.js
@@ -0,0 +1,54 @@
+export const signIn = credentials => {
+  return (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(credentials.email, credentials.password)
+      .then(() => {
+        dispatch({ type: 'LOGIN_SUCCESS' });
+      })
+      .catch(err => {
+        dispatch({ type: 'LOGIN_ERROR', err });
+      });
+  };
+};
+
+export const signOut = () => {
+  return (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        dispatch({ type: 'LOGOUT_SUCCESS' });
+      });
+  };
+};
+
+export const signUp = newUser => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(newUser.email, newUser.password)
+      .then(resp => {
+        // Pass the uid in resp from the auth database
+        return firestore
+          .collection('users')
+          .doc(resp.user.uid)
+          .set({
+            firstName: newUser.firstName,
+            lastName: newUser.lastName,
+            initials: newUser.firstName[0] + newUser.lastName[0]
+          });
+      })
+      .then(() => {
+        dispatch({ type: 'SIGNUP_SUCCESS' });
+      })
+      .catch(err => {
+        dispatch({ type: 'SIGNUP_ERROR' });
+      });
+  };
+};
