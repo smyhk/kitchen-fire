f6e52fb30fd2c97da23b10aef4c34ab4d49092a0
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-11-07 10:38:05 -0800

    Display navbar and links

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // Components
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
+import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateRecipe from './components/recipes/CreateRecipe';
+import RecipeDetails from './components/recipes/RecipeDetails';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
+          <Navbar />
           <Switch>
             <Route exact path="/" component={Dashboard} />
             <Route exact path="/signin" component={SignIn} />
             <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/create" component={CreateRecipe} />
+            <Route exact path="/project/:id" component={RecipeDetails} />
           </Switch>
         </div>
       </BrowserRouter>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = props => {
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
-          MarioPlan
+          KitchenFire
         </Link>
         {links}
       </div>
