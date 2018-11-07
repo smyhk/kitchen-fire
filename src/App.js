5303d4630b9a706fca6c2efdcbdca1a6620eb611
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-11-07 10:03:47 -0800

    Implement signin and signup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 // Components
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 import Dashboard from './components/dashboard/Dashboard';
 
 class App extends Component {
@@ -11,6 +13,8 @@ class App extends Component {
         <div className="App">
           <Switch>
             <Route exact path="/" component={Dashboard} />
+            <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/signup" component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
