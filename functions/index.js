8ba4ca040c89c69a748c75743a0cc7be2d91ac71
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-11-09 10:01:40 -0800

    Implement cloud functions

---
+++ functions/index.js
@@ -0,0 +1,48 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   response.send('Hello from Firebase!');
+// });
+
+const createNotification = notification => {
+  return admin
+    .firestore()
+    .collection('notifications')
+    .add(notification)
+    .then(doc => {
+      console.info('notification added', doc);
+    });
+};
+
+exports.recipeCreated = functions.firestore
+  .document('recipes/{recipeId}')
+  .onCreate(doc => {
+    const recipe = doc.data();
+    const notification = {
+      content: 'Added a new recipe',
+      user: `${recipe.authorFirstName} ${recipe.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    };
+
+    return createNotification(notification);
+  });
+
+exports.userJoined = functions.auth.user().onCreate(user => {
+  return admin
+    .firestore()
+    .collection('users')
+    .doc(user.uid)
+    .get()
+    .then(doc => {
+      const newUser = doc.data();
+      const notification = {
+        content: 'Joined the party',
+        user: `${newUser.firstName} ${newUser.lastName}`,
+        time: admin.firestore.FieldValue.serverTimestamp()
+      };
+
+      return createNotification(notification);
+    });
+});
