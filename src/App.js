db412866344dd64c04d97f69af26343fab2c883b
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-11-12 00:10:48 -0800

    Start building landing page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
+import LandingPage from './components/layout/LandingPage';
 import CreateRecipe from './components/recipes/CreateRecipe';
 import RecipeDetails from './components/recipes/RecipeDetails';
 
@@ -16,7 +17,8 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Switch>
-            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/signin" component={SignIn} />
             <Route exact path="/signup" component={SignUp} />
             <Route exact path="/create" component={CreateRecipe} />
